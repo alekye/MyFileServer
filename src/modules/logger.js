51109ab28c6dfa1feda5098440e36636cbd401e1
Alek Ye <alek.ye@smartaid.cn>
2020-05-27 17:20:22 +0800

    fix pm2 cluster & log4js bug

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,20 +1,24 @@
 const log4js = require("log4js");
+const Config = require("../Utils/Config");
+const appConfig = Config.getAppConfig();
 
-log4js.configure({
+const logConfig = {
+  pm2: appConfig.pm2Cluster,
   appenders: {
-    out: { type: 'console' },
+    out: { type: "console" },
     file: {
       type: "dateFile",
-      filename: "logs/thing.log",
+      filename: "./logs/run.log",
       daysToKeep: 30,
       pattern: ".yyyy-MM-dd",
-      keepFileExt: true
+      keepFileExt: true,
     },
   },
   categories: {
     default: { appenders: ["out", "file"], level: "debug" },
   },
-});
-
-const logger = log4js.getLogger();
-module.exports = logger;
+};
+log4js.configure(logConfig);
+const log = log4js.getLogger();
+log.info("logConfig === ", logConfig);
+module.exports = log;
